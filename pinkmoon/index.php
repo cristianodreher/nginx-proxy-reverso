ad23be7d2a8371aa3190a49ec312254441c516e8
Cristiano <cristianodreher@gmail.com>
2023-09-18 13:54:48 -0300

    Proxy Reverso com Containers sem SSL

---
+++ pinkmoon/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Informações do Contêiner</title>
+    <style>
+        body {
+            background-color: #f7d2d2; /* Cor rosa acizentada */
+        }
+        h1 {
+            color: #ff66b2; /* Cor rosa mais vívida para o cabeçalho */
+        }
+    </style>
+</head>
+<body>
+    <h1>Bem-vindo ao Contêiner Pink Moon</h1>
+    <p>Este site está hospedado dentro de um contêiner chamado: <?php echo gethostname(); ?></p>
+    <p>O Contêiner Pink Moon é a sua fonte para experiências web em ambiente de contêineres!</p>
+</body>
+</html>
+
---
+++ pinkmoonjeans/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Informações do Contêiner</title>
+    <style>
+        body {
+            background-color: #cfd8dc; /* Tonalidade de cinza-azul sem listras */
+            margin: 0; /* Remova margens para evitar espaços em branco */
+            padding: 0; /* Remova preenchimentos para evitar espaços em branco */
+            font-family: Arial, sans-serif; /* Use uma fonte padrão para melhor legibilidade */
+        }
+        h1 {
+            color: #1565c0; /* Cor azul para o cabeçalho */
+        }
+        p {
+            color: #333; /* Cor de texto mais escura para melhor legibilidade */
+        }
+    </style>
+</head>
+<body>
+    <h1>Bem-vindo ao Contêiner Pink Moon Jeans</h1>
+    <p>Este site está hospedado dentro de um contêiner chamado: <?php echo gethostname(); ?></p>
+    <p>O Contêiner Pink Moon Jeans é a sua fonte para experiências web em ambiente de contêineres!</p>
+</body>
+</html>
+
