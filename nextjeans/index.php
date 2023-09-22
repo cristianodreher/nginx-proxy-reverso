2243d9db6ab8e1cc22c20cd0880fe9d446fcd91a
cristianodreher <cristianodreher@gmail.com>
2023-09-22 16:06:53 -0300

    Dockerfile site Next Jeans

---
+++ nextjeans/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Informações do Contêiner</title>
+    <style>
+        body {
+            background-color: #cccccc; /* Cor de fundo cinza */
+        }
+        h1 {
+            color: gray; /* Cor do cabeçalho em cinza */
+        }
+        p {
+            color: gray; /* Cor do texto em cinza */
+        }
+    </style>
+</head>
+<body>
+    <h1>Bem-vindo ao Contêiner Next Jeans</h1>
+    <p>Este site está hospedado dentro de um contêiner chamado: <?php echo gethostname(); ?></p>
+</body>
+</html>
